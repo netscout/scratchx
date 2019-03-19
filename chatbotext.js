39b0144dd529e078a15366a1328008ffe3c6d3a5
netscout <brkang@mentoreet.com>
2019-03-19 14:46:40 +0900

    Initial commit

---
+++ chatbotext.js
@@ -0,0 +1,40 @@
+(function(ext) {
+    // Cleanup function when the extension is unloaded
+    ext._shutdown = function() {};
+
+    // Status reporting code
+    // Use this to report missing hardware, plugin or unsupported browser
+    ext._getStatus = function() {
+        return {status: 2, msg: 'Ready'};
+    };
+
+    ext.get_temp = function(lang, funcName, callback) {
+        var jsonData = new Object();
+        jsonData.Lang = lang;
+        jsonData.FuncName = funcName;
+        fetch('https://stg.moducoding.com/Chatbot/LanguageReference', {
+            method: 'post',
+            headers: {
+                //'Authorization' : `Bearer ${token}`,
+                'Accept': 'application/json, text/plain, */*',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(jsonData)
+        })
+        .then(res => {
+            if(res.ok){
+                callback(res.json());
+            }
+        });
+    };
+
+    // Block and block menu descriptions
+    var descriptor = {
+        blocks: [
+            ['R', '%s 함수의 정의 가져오기', 'get_help', 'c', 'printf'],
+        ]
+    };
+
+    // Register the extension
+    ScratchExtensions.register('Weather extension', descriptor, ext);
+})({});
